5170ee779ab151c2963595f3108a8358f5328360
vzakharchuk <zakharchuk.vadym@gmail.com>
2018-05-06 18:07:23 +0300

    //initial commit

--- src/test/java/TestsSinoptik.java
+++ src/test/java/TestsSinoptik.java
@@ -44,8 +44,7 @@ public class TestsSinoptik extends BaseTestClass {
     }
 
     @Test
-    public void testIsTabOpenedCorrectly(){
-        String currentDay = sinoptikMainPage
+    public void testIsTabOpenedCorrectly(){String currentDay = sinoptikMainPage
                 .selectDayOfWeek(FRIDAY)
                 .getDayNameDisplayed();
         assertEquals(FRIDAY.getName(), currentDay);
